9b3925d4598a34599247205a3be5c67f302c0f3f
Matwey V. Kornilov <matwey@sai.msu.ru>
2016-06-01 21:29:47 +0300

    Add entry list action

--- src/main.c
+++ src/main.c
@@ -3,20 +3,34 @@
 #include <argp.h>
 
 #include <persist-state.h>
+#include <persistable-state-header.h>
 
 struct arguments {
 	char* filename;
+	enum {
+		NONE,
+		LIST
+	} action;
 };
+
+void set_default_args(struct arguments* pargs) {
+	pargs->filename = NULL;
+	pargs->action = NONE;
+}
 
 static char argp_doc[] = "syslog-ng-persist - simple syslog-ng persist file tool";
 static struct argp_option argp_options[] = {
-        { 0 }
+	{"list", 'l', 0,      0, "List existing entries" },
+	{ 0 }
 };
 
 static error_t parse_opt (int key, char *arg, struct argp_state *state) {
 	struct arguments *arguments = state->input;
 
 	switch (key) {
+	case 'l':
+		arguments->action = LIST;
+	break;
 	case ARGP_KEY_ARG:
 		if (state->arg_num >= 1)
 			argp_usage (state);
@@ -39,14 +53,27 @@ int parse_args(int argc, char** argv, struct arguments* parguments) {
 	return argp_parse(&argp, argc, argv, 0, 0, parguments);
 }
 
+void handle_entry(gchar* name, gint entry_size, gpointer entry, gpointer userdata) {
+	printf("%s\n", name);
+}
+
 int main(int argc, char** argv) {
 	struct arguments args;
 
+	set_default_args(&args);
 	if (parse_args(argc, argv, &args)) {
 		return 1;
 	}
 
-	PersistState* state = persist_state_new(args->filename);
+	PersistState* state = persist_state_new(args.filename);
+
+	switch (args.action) {
+	case LIST:
+		persist_state_start_dump(state);
+		persist_state_foreach_entry(state, &handle_entry, NULL);
+	break;
+	default:;
+	}
 
 	persist_state_free(state);
 
